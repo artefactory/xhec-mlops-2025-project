b8eab2ce34940ec4a97b06fd3ae209b1a3276e9e
Shaymaa <shaymaa.bakkass@gmail.com>
2025-10-24 03:03:52 +0200

    adding comments

--- bin/run_services.sh
+++ bin/run_services.sh
@@ -9,3 +9,7 @@ docker build -t mlops-api -f Dockerfile.app .
 docker run -d -p 0.0.0.0:8000:8080 mlops-api
 
 ## the commands to run, to add to the readme (explain that the first is to build the image and the second to run it)
+# docker ps to check if container running
+# curl http://127.0.0.1:8080 to test the get api inside docker app
+# head -n 20 src/modelling/train_flow.py to retrieve the first 20 lines of the train flow file, to check if indeed in docker app
+# ls src/ to list all files under src (all relevant apps for us)
