a6100e9044fdfc34ffb72024fd32d4497a4e5b86
Sheetal-12 <sheetal.popat@somaiya.edu>
2025-10-24 12:30:44 +0200

    Edited files so they are compatible

--- src/modelling/predicting.py
+++ src/modelling/predicting.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import numpy as np
 from pathlib import Path
 from src.modelling.utils import load_pickle
-import numpy as np
 from src.modelling.preprocessing import preprocess_data
 
 
